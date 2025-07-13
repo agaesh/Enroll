45dc38df6ed607b5ea1766481dee444de0cf2be7
agaesh <agaeshk@gmail.com>
2025-07-13 12:35:04 +0800

    refactor the userControoler.js

--- src/api/Controllers/userController.js
+++ src/api/Controllers/userController.js
@@ -3,13 +3,10 @@ const userService = require('../Services/UserService');
 exports.registerUser = async (req, res) => {
   try {
     const newUser = await userService.registerUser(req.body);
-
     res.status(201).json({
       message: 'User registered successfully!',
       userId: newUser.id
     });
-
-
   } catch (error) {
     res.status(error.status || 500).json({ error: error.message || 'Internal Server Error' });
   }
