96a100e1501948b484093a15f2a90004c7d64700
agaesh <agaeshk@gmail.com>
2025-07-13 12:29:38 +0800

    add database configuration using Sequelize

---
+++ src/Config/db.js
@@ -0,0 +1,15 @@
+// db.js
+const { Sequelize } = require('sequelize');
+const config = require('./config').development;
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+  }
+);
+
+module.exports = sequelize;
