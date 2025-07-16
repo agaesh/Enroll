2a303b1597abbd71bffc7b6ada8607ac58ff87ea
agaesh <agaeshk@gmail.com>
2025-07-16 13:55:50 +0800

    added programcourses foreignkey migration

---
+++ src/Database/Migrations/20250716055011-add-ProgramCourse-FK.js
@@ -0,0 +1,23 @@
+'use strict';
+
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('programcourses', {
+      fields: ['parent_id'],
+      type: 'foreign key',
+      name: 'fk_programcourse_parent', // custom name for the FK
+      references: {
+        table: 'ProgramCourses',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint('programcourses', 'fk_programcourse_parent');
+  }
+};
