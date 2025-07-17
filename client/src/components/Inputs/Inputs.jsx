4ae59ced0528d75e4c134be9988b2fa744d6f80a
agaesh <agaeshk@gmail.com>
2025-07-17 11:02:02 +0800

    added custom input called Input.jsx and its css. This input have been used in login page

---
+++ client/src/components/Inputs/Inputs.jsx
@@ -0,0 +1,21 @@
+// src/components/Input/Input.jsx
+import React from 'react';
+import styles from './inputs.module.css';
+
+const Input = ({ label, placeholder, type = "text", value, onChange, name }) => {
+  return (
+    <div className={styles.inputGroup}>
+      {label && <label className={styles.label}>{label}</label>}
+      <input
+        className={styles.input}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        name={name}
+      />
+    </div>
+  );
+};
+
+export default Input;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,37 @@
+// src/pages/Login.jsx
+import React, { useState } from 'react';
+import Input from '../components/Inputs/Inputs';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const handleLogin = (e) => {
+  //   e.preventDefault();
+  //   console.log("Logging in with:", email, password);
+  //   // Add your login logic here
+  // };
+
+  return (
+    <>
+        <Input
+          label="Email"
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+        />
+        <Input
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+        />
+    </>
+  );
+};
+
+export default Login;
