ea9222d1e4a8582146c376d73cbfc29cbffafef6
agaesh <agaeshk@gmail.com>
2025-07-31 10:49:32 +0800

    Created LoginUser method in UserService with basic validation.
    created jwt token and it return to set in the cookie

--- src/api/Services/UserService.js
+++ src/api/Services/UserService.js
@@ -3,6 +3,7 @@ const path = require('path');
 const base = path.resolve(__dirname, '../../../');
 const sequelize = require(path.join(base, 'src', 'config', 'db.js'));
 const { DataTypes } = require('sequelize');
+const jwt = require('jsonwebtoken');
 const User = require('../Models/user')(sequelize, DataTypes);
 
 exports.registerUser = async (body) => {
@@ -15,16 +16,48 @@ exports.registerUser = async (body) => {
     // Call DB or validation logic
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-      // Do not use res here, just throw an error or return a value
       const error = new Error('Email already in use');
       error.status = 409;
       throw error;
     }
-
     const newUser = await User.create({ username, email, password });
     return newUser; // controller will format and send response
   } catch (error) {
     throw error;
   }
 };
+
+exports.LoginUser = async(body)=>{
+  try{
+
+    const email = body.email;
+    const password = body.password;
+    const findUser = await User.findOne({email})
+    const validatePassword = await bcrypt.compare(password, findUser.password)
+
+    if(!findUser){
+      const error = new Error('Email does not exist');
+      error.status = 401;
+      throw error
+    }
+    if(!validatePassword){
+      const error = new Error('Account Not Exist')
+      error.status = 400;
+      throw error
+    }
+
+    const token = jwt.sign({ id: findUser.id, email: findUser.email }, process.env.JWT_SECRET, {
+      expiresIn: '1h'
+    });
+
+     // Return both token and user data (excluding password)
+    return {
+      token,
+      findUser
+    };
+   
+  }catch(error){
+    console.error("this error is =" + error)
+  }
+}
 
