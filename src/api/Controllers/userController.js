da072866f5117ea07c62834f828928bb97f9af67
agaesh <agaeshk@gmail.com>
2025-07-31 11:00:51 +0800

    added LoginUser Controller .. after successful.. it sets the token in the header

--- src/api/Controllers/userController.js
+++ src/api/Controllers/userController.js
@@ -11,3 +11,33 @@ exports.registerUser = async (req, res) => {
     res.status(error.status || 500).json({ error: error.message || 'Internal Server Error' });
   }
 };
+
+exports.LoginUser = async(req,res)=>{
+  try{
+      
+    const token  = await userService.LoginUser(req.body)
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 1000 
+    });
+
+    return res.status(200).json({
+      message: 'Login successful',
+      token
+    });
+
+  }catch(error){
+    res.status(error.status || 500).json({ error: error.message || 'Internal Server Error' });
+  }
+}
+
+exports.ChangePassword = async(req,res) =>{
+  try{
+    const ChangePassword = await userService.ChangePassword(req.body);
+  }catch(error){
+      res.status(error.status || 500).json({ error: error.message || 'Internal Server Error' });
+  }
+}
