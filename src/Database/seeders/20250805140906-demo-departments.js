4b27f72d3bd0eeda10a454154d1bdc31885ae12c
agaesh <agaeshk@gmail.com>
2025-08-06 21:11:01 +0800

    created department seeder

---
+++ src/Database/seeders/20250805140906-demo-departments.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Departments', [
+      {
+        department_name: 'Computer Science',
+        department_code: 'CS101',
+        head_id: 1,
+        email: 'cs@university.edu',
+        phone_number: '123-456-7890',
+        building: 'Block A',
+      },
+      {
+        department_name: 'Electrical Engineering',
+        department_code: 'EE202',
+        head_id: 2,
+        email: 'ee@university.edu',
+        phone_number: '987-654-3210',
+        building: 'Block B',
+      },
+      {
+        department_name: 'Mechanical Engineering',
+        department_code: 'ME303',
+        head_id: 3,
+        email: 'me@university.edu',
+        phone_number: '456-789-0123',
+        building: 'Block C',
+      }
+    ], {
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Departments', null, {});
+  }
+};
