d9850423b0d3439a3a740310cec56a7ac060f68a
agaesh <agaeshk@gmail.com>
2025-08-06 21:10:49 +0800

    created foreignkey relationtionship for Instructor table [Fk] and deparment table [PK]

---
+++ src/Database/Migrations/20250805145527-add-fk-department-to-instructors.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.addConstraint('Instructors', {
+      fields: ['department'],
+      type: 'foreign key',
+      name: 'fk_instructors_department_id', // custom constraint name
+      references: {
+        table: 'Departments',
+        field: 'department_id'
+      },
+      onUpdate: 'SET NULL',
+      onDelete: 'RESTRICT' // can change to CASCADE or RESTRICT
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.removeConstraint('Instructors', 'fk_instructors_department_id');
+  }
+};
