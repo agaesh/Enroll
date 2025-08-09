756ecbc0d0cae816b3f5aa83c1199f77bb328c95
agaesh <agaeshk@gmail.com>
2025-08-09 10:17:20 +0800

    added foreignkey constraint in the programCourse table

---
+++ src/Database/Migrations/20250809014716-add-fk-ProgramCourse-Deparment.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint('ProgramCourses', {
+      fields: ['department_id'],
+      type: 'foreign key',
+      name: 'fk_programcourses_department_id', // custom constraint name
+      references: {
+        table: 'Departments',
+        field: 'department_id' // correct key name in Departments table
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'RESTRICT'
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint(
+      'ProgramCourses',
+      'fk_programcourses_department_id'
+    );
+  }
+};
