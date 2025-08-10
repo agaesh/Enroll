c637f8bb4de95a802129ddcf7ca402358f65c953
agaesh <agaeshk@gmail.com>
2025-08-11 00:37:21 +0800

    added programCourseController .. included model to query the ProgramListing. the getAllPrograms. need to get all the programs.... currently added top page limit req.body filters

--- src/api/Controllers/ProgramCourseController.js
+++ src/api/Controllers/ProgramCourseController.js
@@ -1,4 +1,9 @@
 const ProgramService = require("../Services/ProgramCourseServices")
+const path = require('path');
+const base = path.resolve(__dirname, '../../../');
+const sequelize = require(path.join(base, 'src', 'config', 'db.js'));
+const { DataTypes } = require('sequelize');
+const ProgramCourse = require('../Models/programcourse')(sequelize, DataTypes)
 
 exports.createProgram = async (req, res) => {
   try {
@@ -13,3 +18,46 @@ exports.createProgram = async (req, res) => {
     res.status(error.status || 500).json({ error: error.message || 'Internal Server Error' });
   }
 };
+
+exports.getAllPrograms = async (req, res) => {
+  try {
+    let { top, page, limit } = req.body;
+
+    // Convert string query params to numbers
+    top = parseInt(top);
+    page = parseInt(page);
+    limit = parseInt(limit);
+
+    let queryOptions = {
+      order: [['createdAt', 'DESC']] // latest first
+    };
+
+    if (top) {
+      // Return only top X records
+      queryOptions.limit = top;
+    } 
+    else if (page && limit) {
+      // Apply pagination
+      const offset = (page - 1) * limit;
+      queryOptions.limit = limit;
+      queryOptions.offset = offset;
+    }
+
+    const programs = await ProgramCourse.findAll(queryOptions);
+
+    res.status(200).json({
+      status: 'success',
+      data: programs
+    });
+
+  } catch (error) {
+    res.status(500).json({ 
+      status: 'error',
+      message: error.message 
+    });
+  }
+};
+
+exports.DeleteProgram = async(id)=>{
+   return await ProgramService.DeleteProgram(id)
+}
