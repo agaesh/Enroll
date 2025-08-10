8d4ff8153dbccd1e2bf68d38af673a0140220411
agaesh <agaeshk@gmail.com>
2025-08-11 00:38:49 +0800

    cadded listing route, top Program filtered listing, paginated listing route and also delete route were added

--- src/api/Routes/ProgramRoutes.js
+++ src/api/Routes/ProgramRoutes.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const programController = require('../Controllers/ProgramCourseController');
+const programController = require('../Controllers/ProgramCourseController')
+const { Sequelize, DataTypes } = require('sequelize');
 
 // GET /api/programs
 router.get('/', (req, res) => {
   res.status(200).send("Welcome To Program API");
 });
 
+router.get("/listing", async(req,res)=>{
+    await programController.getAllPrograms(req,res);
+})
+
+router.get("/listing/top/:top", async (req, res) => {
+    await programController.getAllPrograms(req, res);
+});
+
+router.get("/listing/page/:page/limit/:limit", async (req, res) => {
+    await programController.getAllPrograms(req, res);
+});
+
 // POST /api/programs (create program)
-router.post('/', async (req, res) => {
+router.post('/add', async (req, res) => {
   await programController.createProgram(req, res);
 });
 
+// DELETE /delete/:id
+router.delete("/delete", async (req, res) => {
+    try {
+        const {id} = req.body; // ✅ from body
+        const result = await programController.DeleteProgram(id);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message });
+    }
+});
+
+
+module.exports = router
