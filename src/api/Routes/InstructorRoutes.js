df6e5d3d6d8b58bc57d5447a1c039c79dc241080
agaesh <agaeshk@gmail.com>
2025-08-31 20:52:19 +0800

    added instructor route

---
+++ src/api/Routes/InstructorRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const InstructorController = require('../Controllers/InstructorController')
+
+// GET /api/programs
+router.get('/', (req, res) => {
+  res.status(200).send("Welcome To Instructor API");
+});
+
+router.get('/listing', async(req, res)=>{
+  //  await InstructorController.getAllInstructor(req,res)
+})
+// POST /api/deparment (create program)
+router.post('/', async (req, res) => {
+  await InstructorController.CreateInstructor(req, res);
+});
+// PUT /api/deparment (create program)
+router.put('/', async (req, res) => {
+  await InstructorController.UpdateInstructor(req,res)
+});
+
+router.delete('/:id', async (req, res) => {
+  await InstructorController.DeleteInstructor(req,res)
+});
+module.exports = router
