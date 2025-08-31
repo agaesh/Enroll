0ca190deca8422cdd787e6fb7e68225ecc48bd5d
agaesh <agaeshk@gmail.com>
2025-08-31 20:52:56 +0800

    Refactored  user and program routes

--- src/Config/index.js
+++ src/Config/index.js
@@ -10,8 +10,8 @@ require('dotenv').config();
 const UserRegister = require("../api/Services/UserService");
 const ProgramService = require('../api/Services/ProgramCourseServices')
 // Require the router
-const userRoute = require('../api/Routes/userRoute');
-const ProgramRoute = require('../api/Routes/ProgramRoutes');
+const userRoutes = require('../api/Routes/userRoute');
+const ProgramRoutes = require('../api/Routes/ProgramRoutes');
 const departmentRoutes = require('../api/Routes/DepartmentRoutes')
 
 // Example route
@@ -20,8 +20,8 @@ app.get('/', (req, res) => {
 });
 app.use(express.urlencoded({ extended: true }));
 // Use the router at a mount path
-app.use('/users', userRoute);
-app.use('/program', ProgramRoute)
+app.use('/users', userRoutes);
+app.use('/program', ProgramRoutes)
 app.use('/department', departmentRoutes)
 app.listen(port, () => {
     console.log(`Express API listening at http://localhost:${port}`);
