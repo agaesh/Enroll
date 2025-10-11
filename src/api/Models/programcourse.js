0cf5423b8048402a6a33534d445a664e7ef10882
agaesh <agaeshk@gmail.com>
2025-10-11 09:32:14 +0800

    refactor: migrate ProgramCourse model to ES module syntax

--- src/api/Models/programcourse.js
+++ src/api/Models/programcourse.js
@@ -1,60 +1,57 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class ProgramCourse extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+import { Model, DataTypes } from 'sequelize';
 
+export default (sequelize) => {
+  class ProgramCourse extends Model {
+    static associate(models) {
+      // Self-referencing association
       ProgramCourse.belongsTo(models.ProgramCourse, {
-      as: 'parent',
-      foreignKey: 'parent_id'
+        as: 'parent',
+        foreignKey: 'parent_id',
       });
 
       ProgramCourse.hasMany(models.ProgramCourse, {
-      as: 'children',
-      foreignKey: 'parent_id'
+        as: 'children',
+        foreignKey: 'parent_id',
       });
 
       // Department association
       ProgramCourse.belongsTo(models.Department, {
         foreignKey: 'department_id',
-        as: 'department'
+        as: 'department',
       });
     }
   }
-  ProgramCourse.init({
-    parent_id: DataTypes.INTEGER,
-    type: {
-      type:DataTypes.ENUM('PROGRAM','COURSE'),
-      allowNull:false,
-      defaultValue:null,
+
+  ProgramCourse.init(
+    {
+      parent_id: DataTypes.INTEGER,
+      type: {
+        type: DataTypes.ENUM('PROGRAM', 'COURSE'),
+        allowNull: false,
+        defaultValue: null,
+      },
+      code: DataTypes.STRING,
+      name: DataTypes.STRING,
+      department_id: DataTypes.INTEGER,
+      credit_hours: DataTypes.INTEGER,
+      semester: DataTypes.INTEGER,
+      level: {
+        type: DataTypes.ENUM('DIPLOMA', 'DEGREE', 'MASTER', 'DOCTORATE'),
+        allowNull: false,
+      },
+      is_active: DataTypes.BOOLEAN,
+      mode: {
+        type: DataTypes.ENUM('FULLTIME', 'PARTTIME', 'ONLINE', 'HYBRID', 'DISTANCE'),
+        allowNull: false,
+        defaultValue: '',
+      },
     },
-    code: DataTypes.STRING,
-    name: DataTypes.STRING,
-    department_id: DataTypes.INTEGER,
-    credit_hours: DataTypes.INTEGER,
-    semester: DataTypes.INTEGER,
-    level: {
-      type: DataTypes.ENUM('DIPLOMA','DEGREE','MASTER','DOCTORATE'),
-      allowNull:false
-    },
-    is_active: DataTypes.BOOLEAN,
-    mode: {
-    type: DataTypes.ENUM('FULLTIME', 'PARTTIME', 'ONLINE', 'HYBRID', 'DISTANCE'),
-    allowNull: false, // or false if it's required
-    defaultValue:''
-}
-  }, {
-    sequelize,
-    modelName: 'ProgramCourse',
-    timestamps: true
-  });
+    {
+      sequelize,
+      modelName: 'ProgramCourse',
+      timestamps: true,
+    }
+  );
+
   return ProgramCourse;
 };
