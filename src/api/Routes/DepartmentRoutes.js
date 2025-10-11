57d32382e1bbd90364d7dbfa30cb8a36267bc95b
agaesh <agaeshk@gmail.com>
2025-10-11 22:33:19 +0800

    refactor(routes): convert DepartmentRoutes to ES6 module syntax and fix typos

--- src/api/Routes/DepartmentRoutes.js
+++ src/api/Routes/DepartmentRoutes.js
@@ -1,21 +1,25 @@
-const express = require('express');
+import express from 'express';
+import DepartmentController from '../Controllers/DepartmentController.js';
+import { Sequelize, DataTypes } from 'sequelize';
+
 const router = express.Router();
-const DeparmentController = require('../Controllers/DepartmentController')
-const { Sequelize, DataTypes } = require('sequelize');
 
-router.get('/', (req, res)=>{
-    res.status(200).send("Welcome To Deparment API");
-})
+router.get('/', (req, res) => {
+  res.status(200).send('Welcome To Department API');
+});
 
-router.get('/listing', async(req, res)=>{
-   await DeparmentController.getAllDeparments(req,res)
-})
-// POST /api/deparment (create program)
+router.get('/listing', async (req, res) => {
+  await DepartmentController.getAllDeparments(req, res);
+});
+
+// POST /api/department (create department)
 router.post('/', async (req, res) => {
-  await DeparmentController.createDeparment(req, res);
+  await DepartmentController.createDeparment(req, res);
 });
-// PUT /api/deparment (create program)
+
+// PUT /api/department (update department)
 router.put('/', async (req, res) => {
-  await DeparmentController.UpdateDepartment(req,res)
+  await DepartmentController.UpdateDepartment(req, res);
 });
-module.exports = router
+
+export default router;
